c54d249fd0cdf842c4e45884942532f91e6682ca
Boy Maas <boy.maas@gmail.com>
2011-03-10 02:30:48 +0100

    new partials

--- template.rb
+++ template.rb
@@ -1,38 +1,10 @@
 # Check prerequisites
-unless Gem.available?("colored")
-  run "gem install colored"
-  Gem.refresh
-  Gem.activate("colored")
-end
-
-unless Gem.available?("rails")
-  run "gem install rails"
-  Gem.refresh
-  Gem.activate("rails")
-end
-
-unless Gem.available?("bundler")
-  run "gem install bundler"
-  Gem.refresh
-  Gem.activate("bundler")
-end
-
-unless Gem.available?("compass")
-  run "gem install compass"
-  Gem.refresh
-  Gem.activate("compass")
-end
-
-unless Gem.available?("html5-boilerplate")
-  run "gem install html5-boilerplate"
-  Gem.refresh
-  Gem.activate("html5-boilerplate")
-end
-
-unless Gem.available?("haml")
-  run "haml"
-  Gem.refresh
-  Gem.activate("haml")
+[:colored, :rails, :bundler, :compass, :'html5-boilerplate', :haml].map(&:to_s).each do |prerequite|
+  unless Gem.available?(prerequite)
+    run "gem install #{prerequite}"
+    Gem.refresh
+    Gem.activate(prerequite)
+  end
 end
 
 require "colored"
@@ -49,9 +21,11 @@ puts "=========================================================\n"
 
 apply "#{@partials}/gemfile.rb"
 apply "#{@partials}/rvm.rb"           # Must be after gemfile since it runs bundler
+apply "#{@partials}/git.rb"           
 apply "#{@partials}/boilerplate.rb"
 apply "#{@partials}/grid.rb"          # Must be after boilerplate since it modifies SASS files
 apply "#{@partials}/stylesheets.rb"   # Must be after boilerplate since it modifies SASS files
+apply "#{@partials}/jquery.rb"
 apply "#{@partials}/layouts.rb"       # Must be after boilerplate since it modifies HAML files
 apply "#{@partials}/helpers.rb"
 apply "#{@partials}/appconfig.rb"
@@ -59,9 +33,13 @@ apply "#{@partials}/rspec.rb"
 apply "#{@partials}/capistrano.rb"
 apply "#{@partials}/application.rb"
 apply "#{@partials}/friendly_id.rb"   # Must be after application.rb since it runs migrations
-apply "#{@partials}/git.rb"           # Must be last in order to commit initial repository
 apply "#{@partials}/demo.rb"
 apply "#{@partials}/cleanup.rb"
+
+# extra's
+apply "#{@partials}/database.rb"
+apply "#{@partials}/devise.rb"
+apply "#{@partials}/simple_form.rb"
 
 puts "\n========================================================="
 puts " INSTALLATION COMPLETE!".yellow.bold
